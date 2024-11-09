dcdb2eda059c735167f580a3ecf9c0191c53827a
DevGupta <devgupta851900@gmail.com>
2024-11-09 21:24:44 +0530

    updated files

--- src/redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { CartSlice } from "./Slice";
-
-
-export const store=configureStore({
-    reducer:{
-        cart:CartSlice.reducer,
-    },
-})
